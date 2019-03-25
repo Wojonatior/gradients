db76c856cb02f049d80a63b9c3fe0c9d2a796081
Jarek Wojciechowski <jarekwojo@gmail.com>
2019-03-25 10:12:27 -0400

    draw a plain grid of 8 squares

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ var canvas = document.querySelector('canvas');
 var context = canvas.getContext('2d');
 
 const dpr = window.devicePixelRatio;
-const STEP = 40;
 
-canvas.width = window.innerWidth * dpr;
-canvas.height = window.innerHeight * dpr;
+const SIZE = (window.innerWidth / 2) * dpr;
+canvas.width = SIZE;
+canvas.height = SIZE;
 context.scale(dpr, dpr);
+const STEP = SIZE  / 8;
 
 context.lineCap = 'square';
 context.lineWidth = 2;
 const MAX_DISTANCE = Math.sqrt((window.innerWidth * window.innerWidth) + (window.innerHeight * window.innerHeight));
+
+
+for(var x=0; x<=SIZE - STEP; x+=STEP){
+  for(var y=0; y<=SIZE - STEP; y+=STEP){
+    console.log(x, y);
+    context.rect(x, y, x+STEP, y+STEP);
+    context.fillStyle = "rgba(255, 255, 255, 0)";
+    context.stroke();
+  }
+}
