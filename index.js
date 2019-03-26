719e4298ec099b9e6de779085cfb3a361b1cf575
Jarek Wojciechowski <jarekwojo@gmail.com>
2019-03-26 00:24:52 -0400

    add color pallet, vary gradient per square, and add varied gradient border

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
 var canvas = document.querySelector('canvas');
 var context = canvas.getContext('2d');
 
-const dpr = window.devicePixelRatio;
+const DPR = window.devicePixelRatio || 1;
+const SIZE = window.innerWidth * .33;
+const STEP = SIZE  / 11;
+const PALLET = [
+  '#f2d7ee',
+  '#d3bcc0',
+  '#a5668b',
+  '#69306d',
+  '#0e103d'
+]
 
-const SIZE = (window.innerWidth / 2) * dpr;
-canvas.width = SIZE;
-canvas.height = SIZE;
-//context.scale(dpr, dpr);
-const STEP = SIZE  / 9;
-
-context.lineCap = 'square';
+canvas.width = SIZE * DPR;
+canvas.height = SIZE * DPR;
+context.scale(DPR, DPR);
 context.lineWidth = 2;
 
-const getUpTo2x = () => { return STEP * (Math.random() + 1) };
+const get2Colors = () => {
+  const shuffledArray = PALLET.sort( function() { return 0.5 - Math.random() } );
+  return [shuffledArray[0], shuffledArray[1]];
+};
+
+const getGradientScaling = () => { return STEP * (Math.random() + 1) };
 
 const getGradient = () => {
-    var gradient = null;
-    const whiteBlack = Math.random() > .5 ? 'white' : 'black';
-    gradient = context.createLinearGradient(x, y, x+getUpTo2x(), y+getUpTo2x());
-    if(Math.random() < .5) {
-      gradient.addColorStop(0, whiteBlack);
-      gradient.addColorStop(1, 'blue');
-    } else {
-      gradient.addColorStop(1, whiteBlack);
-      gradient.addColorStop(0, 'blue');
-    }
+    const gradient = context.createLinearGradient(x, y, x+getGradientScaling(), y+getGradientScaling());
+    const selectedColors = get2Colors();
+    gradient.addColorStop(0, selectedColors[0]);
+    gradient.addColorStop(1, selectedColors[1]);
     return gradient;
-}
+};
 
 for(var x=STEP; x <= SIZE - STEP + 1; x+=STEP){
   for(var y=STEP; y <= SIZE - STEP + 1; y+=STEP){
-    var gradient = getGradient();
-    context.fillStyle = gradient;
+    context.fillStyle = getGradient();
     context.fillRect(x, y, x+STEP, y+STEP);
-    gradient = null;
-    //context.fill();
+    context.strokeStyle = getGradient();
+    context.strokeRect(x, y, x+STEP, y+STEP);
   }
 }
