fdccb69f75fdbe7971f108a6856fbf15e890a82b
Jarek Wojciechowski <jarekwojo@gmail.com>
2019-03-25 22:57:27 -0400

    working gradient grid b/w

--- index.js
+++ index.js
@@ -6,19 +6,34 @@ const dpr = window.devicePixelRatio;
 const SIZE = (window.innerWidth / 2) * dpr;
 canvas.width = SIZE;
 canvas.height = SIZE;
-context.scale(dpr, dpr);
-const STEP = SIZE  / 8;
+//context.scale(dpr, dpr);
+const STEP = SIZE  / 9;
 
 context.lineCap = 'square';
 context.lineWidth = 2;
-const MAX_DISTANCE = Math.sqrt((window.innerWidth * window.innerWidth) + (window.innerHeight * window.innerHeight));
 
+const getUpTo2x = () => { return STEP * (Math.random() + 1) };
 
-for(var x=0; x<=SIZE - STEP; x+=STEP){
-  for(var y=0; y<=SIZE - STEP; y+=STEP){
-    console.log(x, y);
-    context.rect(x, y, x+STEP, y+STEP);
-    context.fillStyle = "rgba(255, 255, 255, 0)";
-    context.stroke();
+const getGradient = () => {
+    var gradient = null;
+    const whiteBlack = Math.random() > .5 ? 'white' : 'black';
+    gradient = context.createLinearGradient(x, y, x+getUpTo2x(), y+getUpTo2x());
+    if(Math.random() < .5) {
+      gradient.addColorStop(0, whiteBlack);
+      gradient.addColorStop(1, 'blue');
+    } else {
+      gradient.addColorStop(1, whiteBlack);
+      gradient.addColorStop(0, 'blue');
+    }
+    return gradient;
+}
+
+for(var x=STEP; x <= SIZE - STEP + 1; x+=STEP){
+  for(var y=STEP; y <= SIZE - STEP + 1; y+=STEP){
+    var gradient = getGradient();
+    context.fillStyle = gradient;
+    context.fillRect(x, y, x+STEP, y+STEP);
+    gradient = null;
+    //context.fill();
   }
 }
